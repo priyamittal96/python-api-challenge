2483fd0d8fc841066c67f21ea490011a1cd8c3e6
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-01-30 11:38:02 -0800

    API keys not valid, update to template

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0c4f1b9d8bfb2894970201d55789962e"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "AIzaSyD48rzPdQ-y2JwqVCbeSFZ7r3y0qJ9ZRes"
+g_key = "YOUR KEY HERE!"
